74ec457b4fd9f2f6d6186bc55d4f5e0aa63e8d48
rustam <starwarskust@gmail.com>
2025-12-02 16:14:35 +0300

    move the setting of fixed initial parameters to the constructor

--- Minimal/src/PrimaryGeneratorAction.cc
+++ Minimal/src/PrimaryGeneratorAction.cc
@@ -5,6 +5,8 @@ PrimaryGeneratorAction::PrimaryGeneratorAction()
   fParticleGun = new G4ParticleGun();
   fParticleGun->SetParticleDefinition(G4Proton::ProtonDefinition());
   fParticleGun->SetParticleEnergy(20.*GeV);
+  fParticleGun->SetParticlePosition(G4ThreeVector(0, 0, -0.4*m));
+  fParticleGun->SetParticleMomentumDirection(G4ThreeVector(0, 0, 1));
 }
 
 PrimaryGeneratorAction::~PrimaryGeneratorAction()
@@ -15,7 +17,5 @@ PrimaryGeneratorAction::~PrimaryGeneratorAction()
 void PrimaryGeneratorAction::GeneratePrimaries(G4Event* anEvent)
 {
   G4cout << anEvent->GetEventID() << G4endl;
-  fParticleGun->SetParticlePosition(G4ThreeVector(0, 0, -40.*cm));
-  fParticleGun->SetParticleMomentumDirection(G4ThreeVector(0, 0, 1));
   fParticleGun->GeneratePrimaryVertex(anEvent);
 }
